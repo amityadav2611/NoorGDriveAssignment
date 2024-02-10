381399580912ab64463f76e2ed989d346aa338f9
Amit kumar Yadav <74664132+amityadav2611@users.noreply.github.com>
2024-02-10 18:37:12 +0530

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getStatus, initiateProcess } = require('./controller');
+const {initiateProcess } = require('./controller');
 
 router.post('/process', initiateProcess);
 
